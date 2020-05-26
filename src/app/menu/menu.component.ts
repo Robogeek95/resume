23d79bcacfc175fe63017d4c4ca9c6d0b652decb
robogeek <azeezlukman95@gmail.com>
2020-05-26 23:52:46 +0000

    added social icons

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -3,6 +3,7 @@ import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
 import { faUser, faPuzzlePiece, faBox, faAddressBook, faWeight } from '@fortawesome/free-solid-svg-icons';
+import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-menu',
@@ -40,6 +41,20 @@ export class MenuComponent {
     },
   ];
 
+  public social = [
+    {
+      name: 'LinkedIn',
+      icon: faLinkedin
+    },
+    {
+      name: 'Github',
+      icon: faGithub
+    },
+    {
+      name: 'twitter',
+      icon: faTwitter
+    }
+  ]
   public profilePic = 'assets/2174462f28beb19e801818e1e8e478eab672e46a.png'
   constructor(private breakpointObserver: BreakpointObserver) {}
 }
