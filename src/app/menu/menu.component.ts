0b3c3af97a87ef12dc1999b0cc585eb77a67a72e
robogeek <azeezlukman95@gmail.com>
2020-05-27 03:09:47 +0000

    added active indicator

--- src/app/menu/menu.component.ts
+++ src/app/menu/menu.component.ts
@@ -1,60 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
 import { Observable } from 'rxjs';
 import { map, shareReplay } from 'rxjs/operators';
-import { faUser, faPuzzlePiece, faBox, faAddressBook, faWeight } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin, faGithub, faTwitter } from '@fortawesome/free-brands-svg-icons';
+import {
+  faUser,
+  faPuzzlePiece,
+  faBox,
+  faAddressBook,
+  faWeight,
+} from '@fortawesome/free-solid-svg-icons';
+import {
+  faLinkedin,
+  faGithub,
+  faTwitter,
+} from '@fortawesome/free-brands-svg-icons';
 
 @Component({
   selector: 'app-menu',
   templateUrl: './menu.component.html',
   styleUrls: ['./menu.component.scss'],
 })
-export class MenuComponent {
+export class MenuComponent implements OnInit {
   isHandset$: Observable<boolean> = this.breakpointObserver
     .observe(Breakpoints.Handset)
     .pipe(
       map((result) => result.matches),
       shareReplay()
     );
-
+  public selectedIndex: number;
   public links = [
     {
       name: 'About',
-      icon: faUser
+      icon: faUser,
     },
     {
       name: 'Skills',
-      icon: faPuzzlePiece
+      icon: faPuzzlePiece,
     },
     {
       name: 'Portfolio',
-      icon: faBox
+      icon: faBox,
     },
     {
       name: 'Contact',
-      icon: faAddressBook
+      icon: faAddressBook,
     },
     {
       name: 'Services',
-      icon: faWeight
+      icon: faWeight,
     },
   ];
 
   public social = [
     {
       name: 'LinkedIn',
-      icon: faLinkedin
+      icon: faLinkedin,
     },
     {
       name: 'Github',
-      icon: faGithub
+      icon: faGithub,
     },
     {
       name: 'twitter',
-      icon: faTwitter
-    }
-  ]
-  public profilePic = 'assets/2174462f28beb19e801818e1e8e478eab672e46a.png'
+      icon: faTwitter,
+    },
+  ];
+  public profilePic = 'assets/2174462f28beb19e801818e1e8e478eab672e46a.png';
   constructor(private breakpointObserver: BreakpointObserver) {}
+
+  ngOnInit() {
+    const path = window.location.pathname.split('/')[1];
+    if (path !== undefined) {
+      this.selectedIndex = this.links.findIndex(
+        (link) => link.name.toLowerCase() === path.toLowerCase()
+      );
+    }
+  }
 }
